00deb9711b3754789e642729c19509291c096667
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-02 20:38:00 +0300

    Express app setup & integrating GraphQL inside it

---
+++ server.js
@@ -0,0 +1,13 @@
+// all Express logic
+const express = require('express')
+const expressGraphQL = require('express-graphql')   //glue layer b/n express & GraphQL
+
+const app = express()
+
+app.use('/graphql', expressGraphQL({ //tell the Express app that any request asking for "/graphql" we want graphql to handle
+  graphiql: true // dev tool allowing us to make queries against our dev server
+}))
+
+app.listen(4000, () => {
+  console.log('Listening')
+})
