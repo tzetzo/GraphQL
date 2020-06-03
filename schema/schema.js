aafb26ca6a1afd3d9502613cb859985fdc60dfe9
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-03 12:12:39 +0300

    query for company first added to our RootQuery

--- schema/schema.js
+++ schema/schema.js
@@ -32,13 +32,21 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    user: {
+    user: { // query for a user first
       //the field for when we make queries with GraphiQL
       type: UserType, // the root query asks for the above defined UserType
-      args: { id: { type: GraphQLString } }, //the arg we specify for when we make queries with GraphiQL
+      args: { id: { type: GraphQLString } }, //the arg we must specify when we make queries with GraphiQL
       resolve(parentValue, args) { //resolve auto handles Promises
         //fetch data from 3rd party API
         return axios.get(`http://localhost:3000/users/${args.id}`) // args.id will be provided to the query when the query is made
+          .then(resp => resp.data)
+      }
+    },
+    company: { // query for a company first
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then(resp => resp.data)
       }
     }
