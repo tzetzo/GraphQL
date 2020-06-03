0b4fa2cd052147198aabacd72ffd70db54397bc0
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-03 13:05:13 +0300

    associate users with CompanyType

--- schema/schema.js
+++ schema/schema.js
@@ -1,53 +1,74 @@
 // tell GraphQL what the data looks like
 
 const graphql = require("graphql");
-const axios = require('axios')
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const axios = require("axios");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList
+} = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType), // tells GraphQL multiple users will be associated with the company
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType, // our custom type; above type is used
-      resolve(parentValue, args) { // parentValue returns the user object; resolve returns a ref to another piece of data like company in this case
-        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-          .then(res => res.data)
+      resolve(parentValue, args) {
+        // parentValue returns the user object; resolve returns a ref to another piece of data like company in this case
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    user: { // query for a user first
+    user: {
+      // query for a user first
       //the field for when we make queries with GraphiQL
       type: UserType, // the root query asks for the above defined UserType
       args: { id: { type: GraphQLString } }, //the arg we must specify when we make queries with GraphiQL
-      resolve(parentValue, args) { //resolve auto handles Promises
+      resolve(parentValue, args) {
+        //resolve auto handles Promises
         //fetch data from 3rd party API
-        return axios.get(`http://localhost:3000/users/${args.id}`) // args.id will be provided to the query when the query is made
-          .then(resp => resp.data)
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`) // args.id will be provided to the query when the query is made
+          .then(resp => resp.data);
       }
     },
-    company: { // query for a company first
+    company: {
+      // query for a company first
       type: CompanyType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return axios.get(`http://localhost:3000/companies/${args.id}`)
-          .then(resp => resp.data)
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then(resp => resp.data);
       }
     }
   }
